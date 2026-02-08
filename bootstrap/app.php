80fdba8325f4efc731ba940003a1d72932254476
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2026-02-08 03:09:46 +0200

    auth logic for admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-
+use \Illuminate\Auth\Middleware\Authenticate;
+use \Illuminate\Auth\Middleware\RedirectIfAuthenticated;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -11,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'auth' => Authenticate::class,
+            'guest' => RedirectIfAuthenticated::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
