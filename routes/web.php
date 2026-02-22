2afb0dbdbab14a77397f47c654f87194276d34f6
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2026-02-22 02:59:46 +0200

    instructor contoller and dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,27 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Frontend\UserDashboardController;
+use \App\Http\Controllers\Frontend\InstructorDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/* Student Role Users Routes */
+Route::group(['middleware' => ['auth', 'verified' , 'check_role:student'] , 'prefix' => 'student' , 'as' => 'student.'] , function(){
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard' , [UserDashboardController::class , 'index'] )->name('dashboard');
 });
+
+
+
+/* Instructor Role Users Routes */
+Route::group(['middleware' => ['auth', 'verified' , 'check_role:instructor'] , 'prefix' => 'instructor' , 'as' => 'instructor.'] , function(){
+
+    Route::get('/dashboard' , [InstructorDashboardController::class , 'index'] )->name('dashboard');
+});
+
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
@@ -23,3 +30,4 @@ Route::get('/admin/dashboard', function () {
 
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
+
