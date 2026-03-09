ad145ec06e774fe179dbebc9b33881ab3fa47e21
Ezzeldien Magdy <139116899+Ezzeldien-M-Abdalaziz@users.noreply.github.com>
2026-03-09 06:52:07 +0200

    admin templte

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Frontend\UserDashboardController;
 use \App\Http\Controllers\Frontend\InstructorDashboardController;
@@ -9,7 +8,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/* Student Role Users Routes */
+/*
+ * -------------------------------------------------
+  * Student Role Users Routes
+ * -------------------------------------------------
+*/
 Route::group(['middleware' => ['auth', 'verified' , 'check_role:student'] , 'prefix' => 'student' , 'as' => 'student.'] , function(){
 
     Route::get('/dashboard' , [UserDashboardController::class , 'index'] )->name('dashboard');
@@ -17,7 +20,11 @@ Route::group(['middleware' => ['auth', 'verified' , 'check_role:student'] , 'pre
 
 
 
-/* Instructor Role Users Routes */
+/*
+ * -------------------------------------------------
+  * instructor Role Users Routes
+ * -------------------------------------------------
+*/
 Route::group(['middleware' => ['auth', 'verified' , 'check_role:instructor'] , 'prefix' => 'instructor' , 'as' => 'instructor.'] , function(){
 
     Route::get('/dashboard' , [InstructorDashboardController::class , 'index'] )->name('dashboard');
